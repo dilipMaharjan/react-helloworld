c142e78decb6a2ef3ecc973972e1143740f5bcfd
dmaharjan <dilipmaharjann@gmail.com>
2017-07-12 13:18:03 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          Hello World!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
